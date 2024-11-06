05f7e1b044998902e72417d6374eaaf6f9df5664
miccles <michaeldamoulakis@gmail.com>
2024-11-06 00:40:14 -0500

    Added CircleShape class

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from constants import *
 
 
 def main():
-    pygame.init()
     print("Starting asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -18,6 +20,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
